c5f19d19f7aef475cd46a184926e7f7fd43c36fc
Riaño Salazar <d.rianosalazar@dxc.com>
2025-11-12 17:03:12 -0500

    Se crea el recurso para empezar a usar el OCR

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MobilesModule } from './mobiles/mobiles.module';
 
 @Module({
-  imports: [],
+  imports: [MobilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
